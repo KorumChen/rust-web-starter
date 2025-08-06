b0f1e5edd54b3936c9f5e9ac6bc747c041820d48
chenuvi <chenuvi@outlook.com>
2025-08-06 23:11:12 +0800

    feat: add config

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,1 +1,39 @@
 mod server;
+use std::sync::LazyLock;
+
+use anyhow::Context;
+use config::{Config, FileFormat};
+use serde::Deserialize;
+pub use server::ServerConfig;
+
+static CONFIG: LazyLock<AppConfig> =
+    LazyLock::new(|| AppConfig::load().expect("Failed to load application configuration"));
+
+#[derive(Debug, Deserialize)]
+pub struct AppConfig {
+    pub server: ServerConfig,
+}
+
+impl AppConfig {
+    pub fn load() -> anyhow::Result<Self> {
+        Config::builder()
+            .add_source(
+                config::File::with_name("application")
+                    .format(FileFormat::Yaml)
+                    .required(true),
+            )
+            .add_source(
+                config::Environment::with_prefix("APP")
+                    .try_parsing(true)
+                    .separator("_")
+                    .list_separator(","),
+            )
+            .build()?
+            .with_context(|| anyhow::anyhow!("Failed to load configuration"))?
+            .try_deserialize()
+            .with_context(|| anyhow::anyhow!("Failed to deserialize configuration"))
+    }
+}
+pub fn get() {
+    &CONFIG
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use axum::{Router, debug_handler, routing};
 use tokio::net::TcpListener;
 use tracing;
+mod config;
 mod logger;
+
 #[tokio::main]
 async fn main() {
     logger::init();
-    const ADDR: &str = "0.0.0.0:3000";
+
+    let port = config::get().server.port();
+    // const ADDR: &str = "127.0.0.1:3000";
 
     // 创建路由
     let app = Router::new()
@@ -13,11 +17,13 @@ async fn main() {
         .route("/health", routing::get(health_check));
 
     // 绑定地址和端口
-    let listener = TcpListener::bind(ADDR).await.unwrap();
-    tracing::info!("listening on http://{}", ADDR);
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
+    tracing::info!("listening on http://0.0.0.0:{}", port);
 
     // 启动服务器
-    axum::serve(listener, app).await?;
+    axum::serve(listener, app).await.unwrap();
 }
 
 #[debug_handler]
